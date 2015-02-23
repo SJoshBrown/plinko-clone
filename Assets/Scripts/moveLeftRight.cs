27e68a9068723107855faae680fcb9df0b419280
Josh Brown <S.Josh.Brown@Gmail.com>
2015-02-22 18:05:49 -0600

    basket movement and boundary

---
+++ Assets/Scripts/moveLeftRight.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class moveLeftRight : MonoBehaviour {
+
+	public float speed;
+	public float xMin;
+	public float xMax;
+	private float x;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		this.transform.Translate(Input.GetAxis("Horizontal") * speed,0,0);
+
+
+		transform.position = new Vector3(
+        Mathf.Clamp(transform.position.x, xMin, xMax),
+        transform.position.y,
+        transform.position.z
+    );
+	}
+}
