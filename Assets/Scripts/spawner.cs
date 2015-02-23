9f1142a0d1ffb7befd99753cc2800537ec53aeb0
Josh Brown <S.Josh.Brown@gmail.com>
2015-02-22 20:36:34 -0600

    random spawning

---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+	public float[] xpositions = new float[9];
+	public GameObject objectToSpawn;
+	void SpawnObject() {
+		Instantiate (objectToSpawn, new Vector3 (xpositions[Random.Range (0, xpositions.Length)], 3.6f, 0), Quaternion.identity);
+	}
+
+	// Use this for initialization
+	void Start () {
+			InvokeRepeating ("SpawnObject", 0, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
