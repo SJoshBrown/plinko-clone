93174b29fa6e10045ba55eb34d4ec793d57bc834
Josh Brown <S.Josh.Brown@gmail.com>
2015-02-23 14:27:16 -0600

    peg graphics

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -5,13 +5,14 @@ public class spawner : MonoBehaviour {
 
 	public float[] xpositions = new float[9];
 	public GameObject objectToSpawn;
+	private float spawnTime = 2.0f;
 	void SpawnObject() {
 		Instantiate (objectToSpawn, new Vector3 (xpositions[Random.Range (0, xpositions.Length)], 3.6f, 0), Quaternion.identity);
 	}
 
 	// Use this for initialization
 	void Start () {
-			InvokeRepeating ("SpawnObject", 1, 1.0f);
+		InvokeRepeating ("SpawnObject", spawnTime, spawnTime);
 	}
 	
 	// Update is called once per frame
