d26c0f7a925cf13ed3720a55f4d09846739317b4
Josh Brown <S.Josh.Brown@Gmail.com>
2015-02-24 15:11:28 -0600

    drop behavior

---
+++ Assets/Scripts/DropBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.gameObject.transform.position.y <= 3.4f)
+		{
+			Debug.Log("< -2");
+			this.gameObject.rigidbody2D.isKinematic = true;
+		}
+	}
+}
--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -5,21 +5,29 @@ public class spawner : MonoBehaviour {
 
 	public float[] xpositions = new float[9];
 	public GameObject objectToSpawn;
+	public GameObject placeholderObjectToSpawn;
 	public float initialRate = 3.0f;
 	private float changeFromInitialRate = 0.0f;
 	public float spawnTime;
 	private float lastCreated = Time.time;
 	private float now = Time.time;
 	public float minimumInterval = 0.0f;
+	private Transform placeholderPosition;
 
 	//Mathf.Clamp(changeFromInitialRate, 0.0f, 2.0f);
 	void SpawnObject() {
-		Instantiate (objectToSpawn, new Vector3 (xpositions[Random.Range (0, xpositions.Length)], 3.6f, 0), Quaternion.identity);
+		//Instantiate (placeholderObjectToSpawn, new Vector3 (xpositions[Random.Range (0, xpositions.Length)], 3.6f, 0), Quaternion.identity);
+
+		GameObject existingPlaceholder = GameObject.FindWithTag ("ballPlaceholder");
+		placeholderPosition = existingPlaceholder.transform;
+		Instantiate (objectToSpawn, new Vector3 (placeholderPosition.position.x, placeholderPosition.position.y, placeholderPosition.position.z), Quaternion.identity);
+		Destroy (existingPlaceholder);
+		Instantiate (placeholderObjectToSpawn, new Vector3 (xpositions[Random.Range (0, xpositions.Length)], 3.6f, 0), Quaternion.identity);
 	}
 
 	// Use this for initialization
 	void Start () {
-		SpawnObject();
+		Instantiate (placeholderObjectToSpawn, new Vector3 (xpositions[Random.Range (0, xpositions.Length)], 3.6f, 0), Quaternion.identity);
 
 	}
 	
