1c05e929d51eed798e32670a4689fd4e09376391
Josh Brown <S.Josh.Brown@gmail.com>
2015-02-24 13:41:37 -0600

    variable spawn rate

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -5,18 +5,30 @@ public class spawner : MonoBehaviour {
 
 	public float[] xpositions = new float[9];
 	public GameObject objectToSpawn;
-	private float spawnTime = 2.0f;
+	private float initialRate = 3.0f;
+	public float changeFromInitialRate = 0.0f;
+	private float spawnTime;
+	private float lastCreated = Time.time;
 	void SpawnObject() {
 		Instantiate (objectToSpawn, new Vector3 (xpositions[Random.Range (0, xpositions.Length)], 3.6f, 0), Quaternion.identity);
 	}
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("SpawnObject", spawnTime, spawnTime);
+
+
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
-		
+		spawnTime = initialRate - changeFromInitialRate;
+		float now = Time.time;
+
+		if (now - this.lastCreated > this.spawnTime) {
+			this.SpawnObject ();
+			this.lastCreated = now;
+		}
+		changeFromInitialRate += Time.deltaTime/100;
 	}
 }
