a504c515c26420c88c743712d82a3edb528bbe5e
Josh Brown <S.Josh.Brown@Gmail.com>
2015-02-26 17:50:10 -0600

    change enterbasket to BallCollisionBehavior

---
+++ Assets/Scripts/BallCollisionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallCollisionBehavior : MonoBehaviour {
+
+	private GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+	}
+	
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Glove") {
+			Destroy (this.gameObject);
+			gameController.AddScore(1);
+		} else if (collision.gameObject.tag == "bottom") {
+			Destroy (this.gameObject);
+			gameController.KillPlayer();
+		}
+
+	}
+
+}
