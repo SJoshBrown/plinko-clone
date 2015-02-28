21c658a40d2e6352268ded77626888f280b79956
Josh Brown <S.Josh.Brown@gmail.com>
2015-02-28 17:33:51 -0600

    fix bug where glove would keep moving on end game

--- Assets/Scripts/moveLeftRight.cs
+++ Assets/Scripts/moveLeftRight.cs
@@ -10,7 +10,7 @@ public class moveLeftRight : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Input.GetAxis("Horizontal") * speed,0,0);
+		transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime,0,0);
 		
 		transform.position = new Vector3(
         Mathf.Clamp(transform.position.x, xMin, xMax),
